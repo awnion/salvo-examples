d7f41fa1e0937ea3ac85de723df3584a439f5e20
Sergei Blinov <blinovsv@gmail.com>
2024-08-27 18:31:56 +0200

    http/s/1/2/3

--- a7i-api/src/bin/server.rs
+++ a7i-api/src/bin/server.rs
@@ -4,3 +4,30 @@ async fn main() {
 
     let _ = a7i_api::server::server("0.0.0.0:80").await;
 }
+
+#[cfg(test)]
+mod tests {
+    use salvo::prelude::*;
+    use salvo::test::ResponseExt;
+    use salvo::test::TestClient;
+
+    #[tokio::test]
+    async fn test_basic() {
+        let service = Service::new(a7i_api::server::router());
+
+        // let content = TestClient::get("http://127.0.0.1:80")
+        //     .send(&service)
+        //     .await
+        //     .take_string()
+        //     .await
+        //     .unwrap();
+        // assert!(content.contains("hello"));
+        let content = TestClient::get("https://oaoaoao:4433/")
+            .send(&service)
+            .await
+            .take_string()
+            .await
+            .unwrap();
+        assert!(content.contains("hello"));
+    }
+}
--- a7i-api/src/server.rs
+++ a7i-api/src/server.rs
@@ -1,4 +1,5 @@
-use salvo::conn::rustls::{Keycert, RustlsConfig};
+use salvo::conn::rustls::Keycert;
+use salvo::conn::rustls::RustlsConfig;
 use salvo::prelude::*;
 
 #[handler]
@@ -6,20 +7,25 @@ async fn hello() -> &'static str {
     "hello"
 }
 
+pub fn router() -> Router {
+    Router::new().get(hello).post(hello)
+}
+
 pub async fn server(addr: &str) -> () {
-    let router = Router::new().get(hello).post(hello);
-    let service = Service::new(router).hoop(Logger::new());
+    let service = Service::new(router()) //
+        .hoop(Logger::new())
+        .hoop(ForceHttps::new());
 
     let rustls_config = RustlsConfig::new(
         Keycert::new()
-            .key_from_path("./a7i-api/certs/key.pem")
-            .expect("key.pem")
-            .cert_from_path("./a7i-api/certs/cert.pem")
-            .expect("cert.pem"),
+            .cert(include_bytes!("../certs/cert.pem").as_ref())
+            .key(include_bytes!("../certs/key.pem").as_ref()),
     );
 
-    let acceptor = TcpListener::new(addr.to_string())
-        .join(QuinnListener::new(rustls_config, "[::]:4444"))
+    let acceptor = TcpListener::new("127.0.0.1:8081")
+        .rustls(rustls_config.clone())
+        // .join(TcpListener::new("127.0.0.1:8081"))
+        .join(QuinnListener::new(rustls_config, "127.0.0.1:8081"))
         .bind()
         .await;
     Server::new(acceptor).serve(service).await;
